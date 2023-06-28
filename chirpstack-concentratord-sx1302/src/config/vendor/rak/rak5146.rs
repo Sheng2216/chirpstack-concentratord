49ca3c068332ef35800f8d30d03177bfdc86b412
Sheng_L <87348555+Sheng2216@users.noreply.github.com>
2023-06-28 16:49:50 +0800

    Update rak5146.rs

--- chirpstack-concentratord-sx1302/src/config/vendor/rak/rak5146.rs
+++ chirpstack-concentratord-sx1302/src/config/vendor/rak/rak5146.rs
@@ -465,19 +465,21 @@ pub fn new(conf: &config::Configuration) -> Result<Configuration> {
             (false, Port::RAK7391_SLOT2) => "/dev/spidev0.1".to_string(),
             _ => panic!("Unknown configuration!"),
         },
-        sx1302_reset_pin: match conf.gateway.sx1302_reset_pin {
-            // 0 => Some(("/dev/gpiochip0".to_string(), match port.clone() {
-            //     Port::RAK7391_SLOT1 => 17,
-            //     Port::RAK7391_SLOT2 => 6,
-            // })),
-            0 => {
-                let pin = match port {
-                    Port::RAK7391_SLOT1 => 17,
-                    Port::RAK7391_SLOT2 => 6,
-                };
-                Some(("/dev/gpiochip0".to_string(), pin))
-            },
-            _ => Some(("/dev/gpiochip0".to_string(), conf.gateway.sx1302_reset_pin)),
+        // sx1302_reset_pin: match conf.gateway.sx1302_reset_pin {
+        //     0 => Some(("/dev/gpiochip0".to_string(), match port.clone() {
+        //         Port::RAK7391_SLOT1 => 17,
+        //         Port::RAK7391_SLOT2 => 6,
+        //     })),
+        //     _ => Some(("/dev/gpiochip0".to_string(), conf.gateway.sx1302_reset_pin)),
+        // },
+        // sx1302_reset_pin: match conf.gateway.sx1302_reset_pin {
+        //     0 => Some(("/dev/gpiochip0".to_string(), 17)),
+        //     _ => Some(("/dev/gpiochip0".to_string(), conf.gateway.sx1302_reset_pin)),
+        // },
+        sx1302_reset_pin: match (conf.gateway.sx1302_reset_pin, port) {    
+            (0, Port::RAK7391_SLOT1) => Some(("/dev/gpiochip0".to_string(), 17)),
+            (0, Port::RAK7391_SLOT2) => Some(("/dev/gpiochip0".to_string(), 6)),
+            (_,_) => Some(("/dev/gpiochip0".to_string(), conf.gateway.sx1302_reset_pin)),
         },
         sx1302_power_en_pin: match conf.gateway.sx1302_power_en_pin {
             0 => None,
